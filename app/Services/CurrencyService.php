c4dfd210a99d43733b38f3ec09cc1950a764fde7
David Lombera <david.lomberlan@gmail.com>
2023-10-02 18:42:01 -0600

    test: currency service

---
+++ app/Services/CurrencyService.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+class CurrencyService
+{
+    public const RATES = [
+        'usd' => [
+            'eur' => 0.98
+        ]
+    ];
+
+    public function convert(float $amount, string $currencyFrom, string $currencyTo): float
+    {
+        $rate = self::RATES[$currencyFrom][$currencyTo] ?? 0;
+
+        return round($amount * $rate, 2);
+    }
+}
