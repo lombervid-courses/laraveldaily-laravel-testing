a24a14326438607e301691028a3f578287ac507e
David Lombera <david.lomberlan@gmail.com>
2023-10-04 19:01:54 -0600

    test(products): edit form values

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -105,6 +105,18 @@ class ProductTest extends TestCase
         $this->assertSame($product['price'], $lastProduct->price);
     }
 
+    public function test_product_edit_contains_correct_values(): void
+    {
+        $product = Product::factory()->create();
+
+        $response = $this->actingAs($this->admin)->get("/products/{$product->id}/edit");
+
+        $response->assertStatus(200);
+        $response->assertSee("value=\"{$product->name}\"", false);
+        $response->assertSee("value=\"{$product->price}\"", false);
+        $response->assertViewHas('product', $product);
+    }
+
     private function createUser(bool $isAdmin = false): User
     {
         return User::factory()->create([
